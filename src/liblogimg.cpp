74745be62313e8906dc1ae0b33c297b294c8af17
Zorby <58777319+Zorby-dev@users.noreply.github.com>
2022-07-12 16:45:15 +0200

    Debug

--- src/liblogimg.cpp
+++ src/liblogimg.cpp
@@ -1,10 +1,12 @@
 #include "liblogimg.hpp"
 #include "lodepng.h"
+#include <iostream>
 
 Image::Image(const char *path) {
     std::vector<unsigned char> image;
     unsigned width, height;
     unsigned error = lodepng::decode(image, width, height, path);
+    std::cout << error << std::endl;
 
     this->image = image;
     this->width = width;
